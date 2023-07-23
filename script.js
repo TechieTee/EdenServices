5875c142419a410217ef3b6570d1e67cfb22fc9e
Fatima Modupe Bello <mailtomodupe@gmail.com>
2023-07-23 03:29:30 +0100

    Implements index page, begin implementation of home page, adds tab and modal functions

---
+++ script.js
@@ -0,0 +1,45 @@
+// Function to change button color
+  function changeColor() {
+    document.getElementById("homeButton").style.backgroundColor = "#2E8B57";
+    setTimeout(function() {
+      document.getElementById("homeButton").style.backgroundColor = "#03A84E";
+    }, 300);
+    window.location.href = "home.html";
+  }
+  
+
+
+// tab function
+const tabby = document.querySelector('.tabby')
+const tabs = Array.from(tabby.querySelectorAll('.tab'))
+const tabContents = Array.from(tabby.querySelectorAll('.tab-content'))
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', event => {
+    const target = tab.dataset.target
+    const tabContent = tabby.querySelector('#' + target)
+
+    // Selects a tab
+    tabs.forEach(t => t.classList.remove('is-selected'))
+    tab.classList.add('is-selected')
+
+    // Selects the corresponding tab content
+    tabContents.forEach(c => c.classList.remove('is-selected'))
+    tabContent.classList.add('is-selected')
+  })
+})
+
+
+// modal
+const modalButton = document.querySelector('.jsModalButton')
+const modalCloseButton = document.querySelector('.jsModalClose')
+
+modalButton.addEventListener('click', _ => {
+  document.body.classList.add('modal-is-open')
+})
+
+modalCloseButton.addEventListener('click', _ => {
+  document.body.classList.remove('modal-is-open')
+})
+
+
